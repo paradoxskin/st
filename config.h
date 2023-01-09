b45594d2f61b6582ab824ff7f4cc7024e3bb65e9
paradoxskin <1312269430@qq.com>
2023-01-09 20:09:07 +0800

    adjust config

--- config.h
+++ config.h
@@ -5,14 +5,16 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=26:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=22:antialias=true:autohint=true";
+//static char *font = "PICO\\-8:pixelsize=22:style=Regular";
 /* Spare fonts */
 static char *font2[] = {
+	"SauceCodePro Nerd Font:pixelsize=20:autialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
-static int borderpx = 2;
+static int borderpx = 1;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -206,7 +208,7 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
